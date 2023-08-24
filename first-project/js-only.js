65885f11f501da962e1c44785e573cb231bc0f15
MDfarazuddin99 <52372209+MDfarazuddin99@users.noreply.github.com>
2023-08-23 20:06:26 -0700

    section-1 done.

--- first-project/js-only.js
+++ first-project/js-only.js
@@ -3,13 +3,11 @@ const input1 = document.getElementById("num1");
 const input2 = document.getElementById("num2");
 
 function add(num1, num2) {
-  return num1 + num2;
+return num1 + num2;
 }
 
 const my_add = () => {
   console.log(add(Number(input1.value), Number(input2.value)));
 };
 
-
 button.addEventListener("click", my_add);
-
